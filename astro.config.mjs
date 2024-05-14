d5423ce9c45509eb8a076a80083ba03fa9382627
plus-agc-service-yamauchi <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2024-05-14 13:01:29 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: { assets: true },
   site: "https://tachineputa-official.jp/",
   integrations: [sitemap()],
 });
