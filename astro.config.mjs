78881f473fc905bf83b2774c77c19d236c36f8c0
plus-agc <plus.web.dev.team@gmail.com>
2024-09-04 09:32:40 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: { assets: true },
   site: "https://tachineputa-official.jp",
   integrations: [sitemap()],
+  build: {
+    sourcemap: true, // ソースマップを有効にする
+  },
+  // experimentalオプションを削除または修正
 });
