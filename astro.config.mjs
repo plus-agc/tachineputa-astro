ccc25aa5deba9b16e626ae0efa9f45b37f6931bb
plus-agc <yamauchi-t@plus-agc.com>
2025-04-09 16:01:29 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import relativeLinks from "astro-relative-links"; // 追加
+
+// 環境変数でsiteとbaseを切り替える
+const isDemo = process.env.BUILD_TARGET === "demo";
 
 export default defineConfig({
-  site: "https://tachineputa-official.jp",
-  integrations: [sitemap()],
+  site: isDemo
+    ? "https://plus-agc.sakura.ne.jp/plus-demo/tachineputa"
+    : "https://tachineputa-official.jp",
+  base: isDemo ? "/plus-demo/tachineputa/" : "/",
+  integrations: [sitemap(), relativeLinks()], // 変更
   build: {
     sourcemap: true, // ソースマップを有効にする
   },
